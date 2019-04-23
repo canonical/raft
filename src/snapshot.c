3a03e1c3dcf960569bccdc808e1e257f568b5a53
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-04-23 09:12:00 +0200

    Fix iteration variable

--- src/snapshot.c
+++ src/snapshot.c
@@ -8,7 +8,7 @@ void snapshot__close(struct raft_snapshot *s)
     unsigned i;
     raft_configuration_close(&s->configuration);
     for (i = 0; i < s->n_bufs; i++) {
-        raft_free(s->bufs[0].base);
+        raft_free(s->bufs[i].base);
     }
     raft_free(s->bufs);
 }
