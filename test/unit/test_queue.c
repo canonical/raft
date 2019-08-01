462fad14634b8ebe1543d385c40ae381b7f83b7b
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-08-01 13:48:42 +0200

    Cleanup test_queue.c

--- test/unit/test_queue.c
+++ test/unit/test_queue.c
@@ -4,9 +4,11 @@
 
 TEST_MODULE(queue);
 
-/**
- * Helpers
- */
+/******************************************************************************
+ *
+ * Fixture
+ *
+ *****************************************************************************/
 
 struct fixture
 {
@@ -16,289 +18,232 @@ struct fixture
 static void *setup(const MunitParameter params[], void *user_data)
 {
     struct fixture *f = munit_malloc(sizeof *f);
-
     (void)params;
     (void)user_data;
-
     QUEUE_INIT(&f->queue);
-
     return f;
 }
 
 static void tear_down(void *data)
 {
     struct fixture *f = data;
-
     free(f);
 }
 
-struct __item
+struct item
 {
     int value;
     void *queue[2];
 };
 
-/**
- * Initialize and push the given items to the queue. Each item will have a value
- * equal to its index plus one.
- */
-#define __push(F, ITEMS)                               \
+/******************************************************************************
+ *
+ * Helper macros
+ *
+ *****************************************************************************/
+
+/* Initialize and push the given items to the fixture's queue. Each item will
+ * have a value equal to its index plus one. */
+#define PUSH(ITEMS)                              \
+    {                                            \
+        int n_ = sizeof ITEMS / sizeof ITEMS[0]; \
+        int i_;                                  \
+        for (i_ = 0; i_ < n_; i_++) {            \
+            struct item *item = &items[i_];      \
+            item->value = i_ + 1;                \
+            QUEUE_PUSH(&f->queue, &item->queue); \
+        }                                        \
+    }
+
+/* Remove the i'th item among the given ones. */
+#define REMOVE(ITEMS, I) QUEUE_REMOVE(&ITEMS[I].queue)
+
+/******************************************************************************
+ *
+ * Assertions
+ *
+ *****************************************************************************/
+
+/* Assert that the item at the head of the fixture's queue has the given
+ * value. */
+#define ASSERT_HEAD(VALUE)                             \
     {                                                  \
-        int n = sizeof ITEMS / sizeof ITEMS[0];        \
-        int i2;                                        \
-                                                       \
-        for (i2 = 0; i2 < n; i2++) {                   \
-            struct __item *item = &items[i2];          \
-            item->value = i2 + 1;                      \
-            QUEUE_PUSH(&F->queue, &item->queue); \
-        }                                              \
+        queue *head_ = QUEUE_HEAD(&f->queue);          \
+        struct item *item_;                            \
+        item_ = QUEUE_DATA(head_, struct item, queue); \
+        munit_assert_int(item_->value, ==, VALUE);     \
     }
 
-/**
- * Remove the i'th item among the given ones.
- */
-#define __remove(ITEMS, I)                   \
-    {                                        \
-        QUEUE_REMOVE(&ITEMS[I].queue); \
+/* Assert that the item at the tail of the queue has the given value. */
+#define ASSERT_TAIL(VALUE)                             \
+    {                                                  \
+        queue *tail_ = QUEUE_TAIL(&f->queue);          \
+        struct item *item_;                            \
+        item_ = QUEUE_DATA(tail_, struct item, queue); \
+        munit_assert_int(item_->value, ==, VALUE);     \
     }
 
-/**
- * Assert that the item at the head of the queue has the given value.
- */
-#define __assert_head(F, VALUE)                              \
-    {                                                        \
-        queue *head = QUEUE_HEAD(&F->queue);     \
-        struct __item *item;                                 \
-                                                             \
-        item = QUEUE_DATA(head, struct __item, queue); \
-        munit_assert_int(item->value, ==, VALUE);            \
-    }
+/* Assert that the fixture's queue is empty. */
+#define ASSERT_EMPTY munit_assert_true(QUEUE_IS_EMPTY(&f->queue))
 
-/**
- * Assert that the item at the tail of the queue has the given value.
- */
-#define __assert_tail(F, VALUE)                              \
-    {                                                        \
-        queue *tail = QUEUE_TAIL(&F->queue);     \
-        struct __item *item;                                 \
-                                                             \
-        item = QUEUE_DATA(tail, struct __item, queue); \
-        munit_assert_int(item->value, ==, VALUE);            \
-    }
+/* Assert that the fixture's queue is not empty. */
+#define ASSERT_NOT_EMPTY munit_assert_false(QUEUE_IS_EMPTY(&f->queue))
 
-/**
- * Assert that the queue is empty.
- */
-#define __assert_is_empty(F)                                \
-    {                                                       \
-        munit_assert_true(QUEUE_IS_EMPTY(&F->queue)); \
-    }
-
-/**
- * Assert that the queue is not empty.
- */
-#define __assert_is_not_empty(F)                             \
-    {                                                        \
-        munit_assert_false(QUEUE_IS_EMPTY(&F->queue)); \
-    }
-
-/**
+/******************************************************************************
+ *
  * QUEUE_IS_EMPTY
- */
+ *
+ *****************************************************************************/
 
 TEST_SUITE(is_empty);
 
 TEST_SETUP(is_empty, setup);
 TEST_TEAR_DOWN(is_empty, tear_down);
 
-TEST_GROUP(is_empty, success);
-
-TEST_CASE(is_empty, success, yes, NULL)
+TEST_CASE(is_empty, yes, NULL)
 {
     struct fixture *f = data;
-
     (void)params;
-
-    __assert_is_empty(f);
-
+    ASSERT_EMPTY;
     return MUNIT_OK;
 }
 
-TEST_CASE(is_empty, success, no, NULL)
+TEST_CASE(is_empty, no, NULL)
 {
     struct fixture *f = data;
-    struct __item items[1];
-
+    struct item items[1];
     (void)params;
-
-    __push(f, items);
-
-    __assert_is_not_empty(f);
-
+    PUSH(items);
+    ASSERT_NOT_EMPTY;
     return MUNIT_OK;
 }
 
-/**
+/******************************************************************************
+ *
  * QUEUE_PUSH
- */
+ *
+ *****************************************************************************/
 
 TEST_SUITE(push);
 
 TEST_SETUP(push, setup);
 TEST_TEAR_DOWN(push, tear_down);
 
-TEST_GROUP(push, success);
-
-TEST_CASE(push, success, one, NULL)
+TEST_CASE(push, one, NULL)
 {
     struct fixture *f = data;
-    struct __item items[1];
-
+    struct item items[1];
     (void)params;
-
-    __push(f, items);
-
-    __assert_head(f, 1);
-
+    PUSH(items);
+    ASSERT_HEAD(1);
     return MUNIT_OK;
 }
 
-TEST_CASE(push, success, two, NULL)
+TEST_CASE(push, two, NULL)
 {
     struct fixture *f = data;
-    struct __item items[2];
+    struct item items[2];
     int i;
-
     (void)params;
-
-    __push(f, items);
-
+    PUSH(items);
     for (i = 0; i < 2; i++) {
-        __assert_head(f, i + 1);
-
-        __remove(items, i);
+        ASSERT_HEAD(i + 1);
+        REMOVE(items, i);
     }
-
-    __assert_is_empty(f);
-
+    ASSERT_EMPTY;
     return MUNIT_OK;
 }
 
-/**
+/******************************************************************************
+ *
  * QUEUE_REMOVE
- */
+ *
+ *****************************************************************************/
 
 TEST_SUITE(remove);
 
 TEST_SETUP(remove, setup);
 TEST_TEAR_DOWN(remove, tear_down);
 
-TEST_GROUP(remove, success);
-
-TEST_CASE(remove, success, first, NULL)
+TEST_CASE(remove, first, NULL)
 {
     struct fixture *f = data;
-    struct __item items[3];
-
+    struct item items[3];
     (void)params;
-
-    __push(f, items);
-
-    __remove(items, 0);
-
-    __assert_head(f, 2);
-
+    PUSH(items);
+    REMOVE(items, 0);
+    ASSERT_HEAD(2);
     return MUNIT_OK;
 }
 
-TEST_CASE(remove, success, second, NULL)
+TEST_CASE(remove, second, NULL)
 {
     struct fixture *f = data;
-    struct __item items[3];
-
+    struct item items[3];
     (void)params;
-
-    __push(f, items);
-
-    __remove(items, 1);
-
-    __assert_head(f, 1);
-
+    PUSH(items);
+    REMOVE(items, 1);
+    ASSERT_HEAD(1);
     return MUNIT_OK;
 }
 
-TEST_CASE(remove, success, third, NULL)
+TEST_CASE(remove, success, NULL)
 {
     struct fixture *f = data;
-    struct __item items[3];
-
+    struct item items[3];
     (void)params;
-
-    __push(f, items);
-
-    __remove(items, 2);
-
-    __assert_head(f, 1);
-
+    PUSH(items);
+    REMOVE(items, 2);
+    ASSERT_HEAD(1);
     return MUNIT_OK;
 }
 
-/**
+/******************************************************************************
+ *
  * QUEUE_TAIL
- */
+ *
+ *****************************************************************************/
 
 TEST_SUITE(tail);
 
 TEST_SETUP(tail, setup);
 TEST_TEAR_DOWN(tail, tear_down);
 
-TEST_GROUP(tail, success);
-
-TEST_CASE(tail, success, one, NULL)
+TEST_CASE(tail, one, NULL)
 {
     struct fixture *f = data;
-    struct __item items[1];
-
+    struct item items[1];
     (void)params;
-
-    __push(f, items);
-
-    __assert_tail(f, 1);
-
+    PUSH(items);
+    ASSERT_TAIL(1);
     return MUNIT_OK;
 }
 
-TEST_CASE(tail, success, two, NULL)
+TEST_CASE(tail, two, NULL)
 {
     struct fixture *f = data;
-    struct __item items[2];
-
+    struct item items[2];
     (void)params;
-
-    __push(f, items);
-
-    __assert_tail(f, 2);
-
+    PUSH(items);
+    ASSERT_TAIL(2);
     return MUNIT_OK;
 }
 
-TEST_CASE(tail, success, three, NULL)
+TEST_CASE(tail, three, NULL)
 {
     struct fixture *f = data;
-    struct __item items[3];
-
+    struct item items[3];
     (void)params;
-
-    __push(f, items);
-
-    __assert_tail(f, 3);
-
+    PUSH(items);
+    ASSERT_TAIL(3);
     return MUNIT_OK;
 }
 
-/**
+/******************************************************************************
+ *
  * QUEUE_FOREACH
- */
+ *
+ *****************************************************************************/
 
 TEST_SUITE(foreach);
 
@@ -311,13 +256,9 @@ TEST_CASE(foreach, zero, NULL)
     struct fixture *f = data;
     queue *head;
     int count = 0;
-
     (void)params;
-
     QUEUE_FOREACH(head, &f->queue) { count++; }
-
     munit_assert_int(count, ==, 0);
-
     return MUNIT_OK;
 }
 
@@ -325,18 +266,13 @@ TEST_CASE(foreach, zero, NULL)
 TEST_CASE(foreach, one, NULL)
 {
     struct fixture *f = data;
-    struct __item items[1];
+    struct item items[1];
     queue *head;
     int count = 0;
-
     (void)params;
-
-    __push(f, items);
-
+    PUSH(items);
     QUEUE_FOREACH(head, &f->queue) { count++; }
-
     munit_assert_int(count, ==, 1);
-
     return MUNIT_OK;
 }
 
@@ -344,17 +280,12 @@ TEST_CASE(foreach, one, NULL)
 TEST_CASE(foreach, two, NULL)
 {
     struct fixture *f = data;
-    struct __item items[2];
+    struct item items[2];
     queue *head;
     int count = 0;
-
     (void)params;
-
-    __push(f, items);
-
+    PUSH(items);
     QUEUE_FOREACH(head, &f->queue) { count++; }
-
     munit_assert_int(count, ==, 2);
-
     return MUNIT_OK;
 }
