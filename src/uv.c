9e71e53b85deb2cebd20fc7b2452de7781a62a50
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-08-24 15:57:48 +0200

    Fix broken log message

--- src/uv.c
+++ src/uv.c
@@ -58,7 +58,7 @@ static int uvInit(struct raft_io *io,
     /* Detect the I/O capabilities of the underlying file system. */
     rv = uvProbeIoCapabilities(uv->dir, &direct_io, &uv->async_io, errmsg);
     if (rv != 0) {
-        uvErrorf(uv, "probe I/O capabilities: %s", uv_strerror(rv));
+        uvErrorf(uv, "probe I/O capabilities: %s", errmsg);
         rv = RAFT_IOERR;
         goto err;
     }
