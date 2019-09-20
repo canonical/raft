b8aece03b8f2e126417036c1e141a99448794c46
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-09-20 12:02:31 +0200

    Add TEST() macro, a more generic replacement of TEST_CASE()

--- test/lib/runner.h
+++ test/lib/runner.h
@@ -1,7 +1,9 @@
-#include "munit.h"
+/* Convenience macros to reduce munit boiler plate. */
 
 #ifndef TEST_RUNNER_H_
 #define TEST_RUNNER_H_
+
+#include "munit.h"
 
 /* Top-level suites array declaration.
  *
@@ -11,9 +13,12 @@
 extern MunitSuite _main_suites[];
 extern int _main_suites_n;
 
+/* Maximum number of child suites or of test cases, for each suite */
+#define TEST__CAP 128
+
 /* Define the top-level suites array and the main() function of the test. */
 #define TEST_RUNNER(NAME)                                          \
-    MunitSuite _main_suites[64];                                   \
+    MunitSuite _main_suites[TEST__CAP];                            \
     int _main_suites_n = 0;                                        \
                                                                    \
     int main(int argc, char *argv[MUNIT_ARRAY_PARAM(argc + 1)])    \
@@ -21,9 +26,6 @@ extern int _main_suites_n;
         MunitSuite suite = {(char *)"", NULL, _main_suites, 1, 0}; \
         return munit_suite_main(&suite, (void *)NAME, argc, argv); \
     }
-
-/* Maximum number of child suites for or of test cases, for each suite */
-#define TEST__CAP 128
 
 /* Declare and register a new test module #M.
  *
@@ -55,12 +57,17 @@ extern int _main_suites_n;
  *
  * The tests and suites attributes of the next available MunitSuite slot in the
  * _module_suites array will be set to the suite's tests and suites arrays, and
- * the prefix attribute of the slot will be set to /S.
- *
- * All tests in the suite will use the same setup and tear down functions. */
+ * the prefix attribute of the slot will be set to /S. */
 #define TEST_SUITE(S)      \
     TEST_SUITE__DECLARE(S) \
     TEST_SUITE__ADD_CHILD(module, "/" #S, S)
+
+/* Declare and register a new test. */
+#define TEST(S, C, SETUP, TEAR_DOWN, OPTIONS, PARAMS)                \
+    static MunitResult test_##S##_##C(const MunitParameter params[], \
+                                      void *data);                   \
+    TEST__ADD_TO_SUITE(S, C, SETUP, TEAR_DOWN, OPTIONS, PARAMS)      \
+    static MunitResult test_##S##_##C(const MunitParameter params[], void *data)
 
 /* Declare a setup function.
  *
@@ -225,6 +232,17 @@ extern int _main_suites_n;
     }
 
 /* Add a test case to the MunitTest[] array of suite S. */
+#define TEST__ADD_TO_SUITE(S, C, SETUP, TEAR_DOWN, OPTIONS, PARAMS)            \
+    __attribute__((constructor)) static void _##S##_tests_##C##_init(void)     \
+    {                                                                          \
+        MunitTest *tests = _##S##_tests;                                       \
+        int n = _##S##_tests_n;                                                \
+        TEST__SET_IN_ARRAY(tests, n, "/" #C, test_##S##_##C, SETUP, TEAR_DOWN, \
+                           OPTIONS, PARAMS);                                   \
+        _##S##_tests_n = n + 1;                                                \
+    }
+
+/* Add a test case to the MunitTest[] array of suite S. */
 #define TEST_CASE__ADD_TO_SUITE(S, C, PARAMS)                              \
     __attribute__((constructor)) static void _##S##_tests_##C##_init(void) \
     {                                                                      \
@@ -246,6 +264,16 @@ extern int _main_suites_n;
                                 _##S##_setup, _##S##_tear_down, PARAMS);    \
         _##S##_##G##_tests_n = n + 1;                                       \
     }
+
+/* Set the values of the I'th test case slot in the given test array */
+#define TEST__SET_IN_ARRAY(TESTS, I, NAME, FUNC, SETUP, TEAR_DOWN, OPTIONS, \
+                           PARAMS)                                          \
+    TESTS[I].name = NAME;                                                   \
+    TESTS[I].test = FUNC;                                                   \
+    TESTS[I].setup = SETUP;                                                 \
+    TESTS[I].tear_down = TEAR_DOWN;                                         \
+    TESTS[I].options = OPTIONS;                                             \
+    TESTS[I].parameters = PARAMS
 
 /* Set the values of the I'th test case slot in the given test array */
 #define TEST_CASE__SET_IN_ARRAY(TESTS, I, NAME, FUNC, SETUP, TEAR_DOWN, \
