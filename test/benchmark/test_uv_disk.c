80a1eb4b94ebb0642acfa224bd33e9c74d2aca23
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-09-24 14:13:32 +0200

    Move file allocation to setup function

--- test/benchmark/test_uv_disk.c
+++ test/benchmark/test_uv_disk.c
@@ -9,39 +9,79 @@
 #define BLOCK_SIZE_ 4096
 #define SEGMENT_SIZE (8 * 1024 * 1024)
 
+/******************************************************************************
+ *
+ * Synchronous writes
+ *
+ *****************************************************************************/
+
 SUITE(synWrite)
 
-TEST(synWrite, append, setupDir, tearDownDir, 0, dir_all_params)
+struct file
 {
-    const char *dir = data;
+    FIXTURE_DIR;
     int fd;
-    void *buf;
+};
+
+static void *setupFile(MUNIT_UNUSED const MunitParameter params[],
+                       MUNIT_UNUSED void *user_data)
+{
+    struct file *f = munit_malloc(sizeof *f);
+    int flags = O_WRONLY | O_CREAT | O_DIRECT | O_DSYNC;
     uvErrMsg errmsg;
+    int rv;
+    SETUP_DIR;
+    if (f->dir == NULL) {
+        free(f);
+        return NULL;
+    }
+    rv = uvOpenFile(f->dir, "x", flags, &f->fd, errmsg);
+    munit_assert_int(rv, ==, 0);
+    rv = posix_fallocate(f->fd, 0, SEGMENT_SIZE);
+    munit_assert_int(rv, ==, 0);
+    rv = fsync(f->fd);
+    munit_assert_int(rv, ==, 0);
+    return f;
+}
+
+static void tearDownFile(void *data)
+{
+    struct file *f = data;
+    if (f == NULL) {
+        return;
+    }
+    close(f->fd);
+    TEAR_DOWN_DIR;
+}
+
+char *appendN[] = {"1", "16", "256", "1024", NULL};
+
+static MunitParameterEnum appendParams[] = {
+    {"n", appendN},
+    {TEST_DIR_FS, test_dir_all},
+    {NULL, NULL},
+};
+
+TEST(synWrite, append, setupFile, tearDownFile, 0, appendParams)
+{
+    struct file *f = data;
+    const char *n = munit_parameters_get(params, "n");
+    void *buf;
     int rv;
     int i;
 
-    if (dir == NULL) {
-        return MUNIT_SKIP;
-    }
+    SKIP_IF_NO_FIXTURE;
 
-    rv = uvOpenFile(dir, "test", O_WRONLY | O_CREAT | O_EXCL, &fd, errmsg);
-    munit_assert_int(rv, ==, 0);
-    rv = posix_fallocate(fd, 0, SEGMENT_SIZE);
-    munit_assert_int(rv, ==, 0);
-    rv = fsync(fd);
-    munit_assert_int(rv, ==, 0);
+    buf = aligned_alloc(BLOCK_SIZE_, BLOCK_SIZE_);
+    munit_assert_ptr_not_null(buf);
 
-    buf = munit_malloc(BLOCK_SIZE_);
-
-    for (i = 0; i < SEGMENT_SIZE / BLOCK_SIZE_; i++) {
+    for (i = 0; i < atoi(n); i++) {
         memset(buf, i, BLOCK_SIZE_);
-        rv = uvWriteFully(fd, buf, BLOCK_SIZE_, errmsg);
-        munit_assert_int(rv, ==, 0);
-        rv = fdatasync(fd);
-        munit_assert_int(rv, ==, 0);
+        rv = write(f->fd, buf, BLOCK_SIZE_);
+        munit_assert_int(rv, ==, BLOCK_SIZE_);
     }
 
     free(buf);
-    close(fd);
+
     return MUNIT_OK;
 }
