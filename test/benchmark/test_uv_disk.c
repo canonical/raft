cbbb41c3e0d5af7e000ed7f1b4cd022f9f934677
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-10-18 22:33:30 +0200

    Add benchmark for synchronous disk I/O

--- test/benchmark/test_uv_disk.c
+++ test/benchmark/test_uv_disk.c
@@ -7,7 +7,7 @@
 #include "../lib/runner.h"
 
 #define BLOCK_SIZE_ 4096
-#define BUF_SIZE BLOCK_SIZE_
+#define BUF_SIZE 4096
 #define SEGMENT_SIZE (8 * 1024 * 1024)
 
 struct file
@@ -20,7 +20,7 @@ static void *setupFile(MUNIT_UNUSED const MunitParameter params[],
                        MUNIT_UNUSED void *user_data)
 {
     struct file *f = munit_malloc(sizeof *f);
-    int flags = O_WRONLY | O_CREAT | O_DIRECT | O_DSYNC;
+    int flags = O_WRONLY | O_CREAT;
     uvErrMsg errmsg;
     int rv;
     SETUP_DIR;
@@ -47,23 +47,6 @@ static void tearDownFile(void *data)
     TEAR_DOWN_DIR;
 }
 
-/******************************************************************************
- *
- * Synchronous writes with direct I/O.
- *
- *****************************************************************************/
-
-#define SET_DIO                                                   \
-    {                                                             \
-        int flags_;                                               \
-        int rv_;                                                  \
-        flags_ = fcntl(f->fd, F_GETFL);                           \
-        rv_ = fcntl(f->fd, F_SETFL, flags_ | O_DIRECT | O_DSYNC); \
-        munit_assert_int(rv_, ==, 0);                             \
-    }
-
-SUITE(dioWrite)
-
 static char *appendN[] = {"1", "16", "256", "1024", NULL};
 
 static char *dirFs[] = {"ext4", "btrfs", "xfs", "zfs", NULL};
@@ -73,6 +56,24 @@ static MunitParameterEnum appendParams[] = {
     {TEST_DIR_FS, dirFs},
     {NULL, NULL},
 };
+
+/******************************************************************************
+ *
+ * Synchronous writes with direct I/O.
+ *
+ *****************************************************************************/
+
+#define SET_DIO                                                 \
+    {                                                           \
+        int flags_ = O_WRONLY | O_DIRECT | O_DSYNC;             \
+        uvErrMsg errmsg_;                                       \
+        int rv_;                                                \
+        close(f->fd);                                           \
+        rv_ = uvOpenFile(f->dir, "x", flags_, &f->fd, errmsg_); \
+        munit_assert_int(rv_, ==, 0);                           \
+    }
+
+SUITE(dioWrite)
 
 TEST(dioWrite, append, setupFile, tearDownFile, 0, appendParams)
 {
@@ -90,8 +91,41 @@ TEST(dioWrite, append, setupFile, tearDownFile, 0, appendParams)
 
     for (i = 0; i < atoi(n); i++) {
         memset(buf, i, BUF_SIZE);
-        rv = write(f->fd, buf, BUF_SIZE);
+        rv = pwrite(f->fd, buf, BUF_SIZE, BUF_SIZE * i);
         munit_assert_int(rv, ==, BUF_SIZE);
+    }
+
+    free(buf);
+
+    return MUNIT_OK;
+}
+
+/******************************************************************************
+ *
+ * Synchronous writes with buffered I/O.
+ *
+ *****************************************************************************/
+
+SUITE(ioWrite)
+
+TEST(ioWrite, append, setupFile, tearDownFile, 0, appendParams)
+{
+    struct file *f = data;
+    const char *n = munit_parameters_get(params, "n");
+    void *buf;
+    int rv;
+    int i;
+
+    SKIP_IF_NO_FIXTURE;
+
+    buf = munit_malloc(BUF_SIZE);
+
+    for (i = 0; i < atoi(n); i++) {
+        memset(buf, i, BUF_SIZE);
+        rv = pwrite(f->fd, buf, BUF_SIZE, BUF_SIZE * i);
+        munit_assert_int(rv, ==, BUF_SIZE);
+        rv = fdatasync(f->fd);
+        munit_assert_int(rv, ==, 0);
     }
 
     free(buf);
