b4f6319fd846b433c9143b91137ca9f480b2c2ef
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-10-24 11:52:16 +0200

    Loop through all engines by default

--- benchmark/os_disk_write.c
+++ benchmark/os_disk_write.c
@@ -13,19 +13,39 @@
 
 static char doc[] = "Benchmark operating system disk write performance";
 
+/* Minimum buffer size to benchmark. */
+#define MIN_BUF_SIZE 64
+
+/* Maximum buffer size to benchmark. */
+#define MAX_BUF_SIZE 4096
+
+/* Minimum physical block size for direct I/O that we expect to detect. */
+#define MIN_BLOCK_SIZE 512
+
+/* Maximum physical block size for direct I/O that we expect to detect. */
+#define MAX_BLOCK_SIZE 4096
+
+/* Engines */
 #define PWRITEV2 0
 #define URING 1
 
+/* Modes */
+#define BUFFERED 0
+#define DIRECT 1
+
 static const char *engines[] =
     {[PWRITEV2] = "pwritev2", [URING] = "uring", NULL};
+
+static const char *modes[] =
+    {[BUFFERED] = "buffered", [DIRECT] = "direct", NULL};
 
 /* Order of fields: {NAME, KEY, ARG, FLAGS, DOC, GROUP}.*/
 static struct argp_option options[] = {
     {"dir", 'd', "DIR", 0, "Directory to use for temp files (default /tmp)", 0},
     {"buf", 'b', "BUF", 0, "Write buffer size (default st_blksize)", 0},
     {"writes", 'n', "N", 0, "Number of writes to perform (default 1024)", 0},
-    {"engine", 'e', "ENGINE", 0, "I/O engine to use (default pwritev2)", 0},
-    {"direct", 'D', NULL, 0, "Use direct I/O", 0},
+    {"engine", 'e', "ENGINE", 0, "I/O engine to use (default all)", 0},
+    {"mode", 'm', "MODE", 0, "Use 'buffered' or 'direct' I/O", 0},
     {0}};
 
 struct arguments
@@ -34,7 +54,7 @@ struct arguments
     int buf;
     int n;
     int engine;
-    bool direct;
+    int mode;
 };
 
 static int engineCode(const char *engine)
@@ -42,6 +62,18 @@ static int engineCode(const char *engine)
     int i = 0;
     while (engines[i] != NULL) {
         if (strcmp(engines[i], engine) == 0) {
+            return i;
+        }
+        i++;
+    }
+    return -1;
+}
+
+static int modeCode(const char *mode)
+{
+    int i = 0;
+    while (modes[i] != NULL) {
+        if (strcmp(modes[i], mode) == 0) {
             return i;
         }
         i++;
@@ -68,8 +100,11 @@ static error_t argumentsParse(int key, char *arg, struct argp_state *state)
                 return ARGP_ERR_UNKNOWN;
             }
             break;
-        case 'D':
-            arguments->direct = true;
+        case 'm':
+            arguments->mode = modeCode(arg);
+            if (arguments->mode == -1) {
+                return ARGP_ERR_UNKNOWN;
+            }
             break;
         default:
             return ARGP_ERR_UNKNOWN;
@@ -86,12 +121,19 @@ static char *makeTempFileTemplate(const char *dir)
     return path;
 }
 
-static int allocateTempFile(int fd, const char *dir, int n_writes, int buf)
+static int createTempFile(const char *dir, int size, char **path, int *fd)
 {
     int dirfd;
     int rv;
 
-    rv = posix_fallocate(fd, 0, n_writes * buf);
+    *path = makeTempFileTemplate(dir);
+    *fd = mkstemp(*path);
+    if (*fd == -1) {
+        printf("mstemp '%s': %s\n", *path, strerror(errno));
+        return -1;
+    }
+
+    rv = posix_fallocate(*fd, 0, size);
     if (rv != 0) {
         errno = rv;
         printf("posiz_fallocate: %s\n", strerror(errno));
@@ -99,7 +141,7 @@ static int allocateTempFile(int fd, const char *dir, int n_writes, int buf)
     }
 
     /* Sync the file and its directory. */
-    rv = fsync(fd);
+    rv = fsync(*fd);
     assert(rv == 0);
 
     dirfd = open(dir, O_RDONLY | O_DIRECTORY);
@@ -109,6 +151,67 @@ static int allocateTempFile(int fd, const char *dir, int n_writes, int buf)
     assert(rv == 0);
 
     close(dirfd);
+
+    return 0;
+}
+
+/* Allocate a buffer of the given size. */
+static void allocBuffer(struct iovec *iov, int size)
+{
+    iov->iov_len = size;
+    iov->iov_base = aligned_alloc(iov->iov_len, iov->iov_len);
+    assert(iov->iov_base != NULL);
+}
+
+static void setDirectIO(int fd)
+{
+    int flags; /* Current fcntl flags */
+    int rv;
+    flags = fcntl(fd, F_GETFL);
+    rv = fcntl(fd, F_SETFL, flags | O_DIRECT);
+    assert(rv == 0);
+}
+
+/* Detect all suitable block size we can use to write to the underlying device
+ * using direct I/O. */
+static int detectSuitableBlockSizesForDirectIO(const char *dir,
+                                               int **block_size,
+                                               int *n_block_size)
+{
+    char *path;
+    int fd;
+    int size;
+    int rv;
+
+    rv = createTempFile(dir, MAX_BLOCK_SIZE, &path, &fd);
+    if (rv != 0) {
+        unlink(path);
+        return -1;
+    }
+
+    setDirectIO(fd);
+
+    *block_size = NULL;
+    *n_block_size = 0;
+
+    for (size = MIN_BLOCK_SIZE; size <= MAX_BLOCK_SIZE; size *= 2) {
+        struct iovec iov;
+        allocBuffer(&iov, size);
+        rv = pwritev2(fd, &iov, 1, 0, RWF_DSYNC | RWF_HIPRI);
+        free(iov.iov_base);
+        if (rv == -1) {
+            assert(errno == EINVAL);
+            continue; /* Try with a bigger buffer size */
+        }
+        assert(rv == size);
+        *n_block_size += 1;
+        *block_size = realloc(*block_size, *n_block_size * sizeof **block_size);
+        assert(*block_size != NULL);
+        (*block_size)[*n_block_size - 1] = size;
+    }
+
+    close(fd);
+    unlink(path);
 
     return 0;
 }
@@ -134,23 +237,6 @@ static int timeSince(struct timespec *start)
                 start->tv_nsec + now.tv_nsec;
     }
     return nsecs / 1000;
-}
-
-/* Allocate a buffer of the given size. */
-static void allocBuffer(struct iovec *iov, int size)
-{
-    iov->iov_len = size;
-    iov->iov_base = aligned_alloc(iov->iov_len, iov->iov_len);
-    assert(iov->iov_base != NULL);
-}
-
-static void setDirectIO(int fd)
-{
-    int flags; /* Current fcntl flags */
-    int rv;
-    flags = fcntl(fd, F_GETFL);
-    rv = fcntl(fd, F_SETFL, flags | O_DIRECT);
-    assert(rv == 0);
 }
 
 static int writeWithPwriteV2(int fd, struct iovec *iov, int i)
@@ -210,11 +296,12 @@ static int writeWithUring(int fd, struct iovec *iov, int i)
     return 0;
 }
 
+/* Benchmark the performance of a single disk write. */
 int benchmarkWritePerformance(const char *dir,
                               int buf,
                               int n,
                               int engine,
-                              bool direct)
+                              int mode)
 {
     char *path;
     int fd;
@@ -223,14 +310,7 @@ int benchmarkWritePerformance(const char *dir,
     int i;
     int rv;
 
-    path = makeTempFileTemplate(dir);
-    fd = mkstemp(path);
-    if (fd == -1) {
-        printf("mstemp '%s': %s\n", path, strerror(errno));
-        return -1;
-    }
-
-    rv = allocateTempFile(fd, dir, n, buf);
+    rv = createTempFile(dir, n * buf, &path, &fd);
     if (rv != 0) {
         unlink(path);
         return -1;
@@ -238,7 +318,7 @@ int benchmarkWritePerformance(const char *dir,
 
     allocBuffer(&iov, buf);
 
-    if (direct) {
+    if (mode == DIRECT) {
         setDirectIO(fd);
     }
 
@@ -261,8 +341,11 @@ int benchmarkWritePerformance(const char *dir,
     }
 
     printf("%-8s:  %8s writes of %4d bytes take %4d microsecs on average\n",
-           engines[engine], direct ? "direct" : "buffered", buf,
-           timeSince(&start) / n);
+           engines[engine], modes[mode], buf, timeSince(&start) / n);
+
+    if (engine == URING) {
+        io_uring_queue_exit(&uring);
+    }
 
     close(fd);
     unlink(path);
@@ -271,18 +354,37 @@ int benchmarkWritePerformance(const char *dir,
     return 0;
 }
 
+static bool isSuitableBufSizeForDirectIO(int *block_size,
+                                         int n_block_size,
+                                         int buf)
+{
+    int i;
+    for (i = 0; i < n_block_size; i++) {
+        if (block_size[i] == buf) {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main(int argc, char *argv[])
 {
     struct argp argp = {options, argumentsParse, NULL, doc, 0, 0, 0};
     struct arguments arguments;
     struct stat st;
+    int engine;
+    int mode;
+    int buf;
+    int *block_size;
+    int n_block_size;
+    int i;
     int rv;
 
     arguments.dir = "/tmp";
-    arguments.buf = 0;
+    arguments.buf = -1;
     arguments.n = 1024;
-    arguments.engine = PWRITEV2;
-    arguments.direct = false;
+    arguments.engine = -1;
+    arguments.mode = -1;
 
     argp_parse(&argp, argc, argv, 0, 0, &arguments);
 
@@ -292,14 +394,51 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    if (arguments.buf == 0) {
-        arguments.buf = st.st_blksize;
+    rv = detectSuitableBlockSizesForDirectIO(arguments.dir, &block_size,
+                                             &n_block_size);
+    if (rv != 0) {
+        return rv;
     }
 
-    rv = benchmarkWritePerformance(arguments.dir, arguments.buf, arguments.n,
-                                   arguments.engine, arguments.direct);
-    if (rv != 0) {
-        return -1;
+    if (arguments.buf != -1) {
+        if (arguments.mode == -1 || arguments.mode == DIRECT) {
+            if (!isSuitableBufSizeForDirectIO(block_size, n_block_size,
+                                              arguments.buf)) {
+                printf("suitable buffer sizes for direct I/O:");
+                for (i = 0; i < n_block_size; i++) {
+                    printf(" %4d", block_size[i]);
+                }
+                printf("\n");
+                return -1;
+            }
+        }
+    }
+
+    for (engine = PWRITEV2; engine <= URING; engine++) {
+        if (arguments.engine != engine && arguments.engine != -1) {
+            continue;
+        }
+        for (mode = BUFFERED; mode <= DIRECT; mode++) {
+            if (arguments.mode != mode && arguments.mode != -1) {
+                continue;
+            }
+            for (buf = MIN_BUF_SIZE; buf <= MAX_BUF_SIZE; buf *= 2) {
+                if (arguments.buf != buf && arguments.buf != -1) {
+                    continue;
+                }
+                if (mode == DIRECT) {
+                    if (!isSuitableBufSizeForDirectIO(block_size, n_block_size,
+                                                      buf)) {
+                        continue;
+                    }
+                }
+                rv = benchmarkWritePerformance(arguments.dir, buf, arguments.n,
+                                               engine, mode);
+                if (rv != 0) {
+                    return -1;
+                }
+            }
+        }
     }
 
     return 0;
