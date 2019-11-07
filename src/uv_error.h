e9ed066556ce61519ef5b43ea38b1f4f6b7e64df
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-11-07 13:15:32 +0100

    Remove unused constants

--- src/uv_error.h
+++ src/uv_error.h
@@ -12,9 +12,6 @@ enum {
     UV__ERROR = 1, /* Generic system error. */
     UV__CANCELED,  /* Request was canceled */
     UV__NODATA,    /* Short read */
-    UV__NOENT,     /* File does not exist */
-    UV__NOTSUPP,   /* Operation not supported */
-    UV__AGAIN      /* Try again later */
 };
 
 /* Format an error message caused by a failed system call or stdlib function. */
