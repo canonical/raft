ee8264ed3f958fff6be201fa05d1d5f1db47488b
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-11-07 13:13:25 +0100

    Remove unused macros

--- src/uv_error.h
+++ src/uv_error.h
@@ -17,31 +17,11 @@ enum {
     UV__AGAIN      /* Try again later */
 };
 
-/* Maximum length of an error message. */
-#define UV__ERRMSG_MAX_LEN 2048
-
-/* Convenience for declaring a char buffer holding an error message. */
-typedef char uvErrMsg[UV__ERRMSG_MAX_LEN];
-
-/* Format an error message, truncating it if it's too long. */
-#define uvErrMsgPrintf(ERRMSG, ...) \
-    snprintf(ERRMSG, UV__ERRMSG_MAX_LEN, __VA_ARGS__);
-
-/* Format an error message caused by a failed system call or stdlib function. */
-#define uvErrMsgSys(ERRMSG, SYSCALL, ERRNUM)                                 \
-    {                                                                        \
-        char e_[1024];                                                       \
-        sprintf(ERRMSG, #SYSCALL ": %s", strerror_r(ERRNUM, e_, sizeof e_)); \
-    }
-
 /* Format an error message caused by a failed system call or stdlib function. */
 #define UvErrMsgSys(ERRMSG, SYSCALL, ERRNUM)             \
     {                                                    \
         ErrMsgPrintf(ERRMSG, "%s", uv_strerror(ERRNUM)); \
         ErrMsgWrapf(ERRMSG, SYSCALL);                    \
     }
-
-/* Format an error message caused by a failed system call or stdlib function. */
-#define uvSysErrMsg(FUNC, CODE) errMsgPrintf(FUNC ": %s", uv_strerror(CODE))
 
 #endif /* UV_ERROR_H_ */
