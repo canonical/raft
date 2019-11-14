44a035b9316fbd09f3811c6a5e60975a2348031a
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-11-14 14:28:30 -0800

    Clean up raft_uv_tcp integration tests

--- test/integration/test_uv_tcp_connect.c
+++ test/integration/test_uv_tcp_connect.c
@@ -8,41 +8,33 @@
 
 /******************************************************************************
  *
- * Fixture
+ * Fixture with a pristine TCP-based raft_uv_transport.
  *
  *****************************************************************************/
 
 struct fixture
 {
-    struct raft_heap heap;
-    struct test_tcp tcp;
+    FIXTURE_HEAP;
     FIXTURE_LOOP;
+    struct test_tcp tcp;
     struct raft_uv_transport transport;
     bool closed;
-    struct raft_uv_connect req;
-    int invoked;
-    int status;
-    struct uv_stream_s *stream;
 };
 
-static void *setup(const MunitParameter params[], void *user_data)
+static void *setUp(const MunitParameter params[], void *user_data)
 {
     struct fixture *f = munit_malloc(sizeof *f);
     (void)user_data;
-    test_heap_setup(params, &f->heap);
-    test_tcp_setup(params, &f->tcp);
+    SETUP_HEAP;
     SETUP_LOOP;
+    test_tcp_setup(params, &f->tcp);
     raft_uv_tcp_init(&f->transport, &f->loop);
     f->transport.config(&f->transport, 1, "127.0.0.1:9000");
     f->closed = false;
-    f->req.data = f;
-    f->invoked = 0;
-    f->status = -1;
-    f->stream = NULL;
     return f;
 }
 
-static void tear_down(void *data)
+static void tearDown(void *data)
 {
     struct fixture *f = data;
     if (!f->closed) {
@@ -62,42 +54,115 @@ static void tear_down(void *data)
  *
  *****************************************************************************/
 
-static void connect_cb(struct raft_uv_connect *req,
-                       struct uv_stream_s *stream,
-                       int status)
+struct connectResult
 {
-    struct fixture *f = req->data;
-    f->invoked++;
-    f->status = status;
-    f->stream = stream;
+    int status;
+    const char *errmsg;
+    bool done;
+};
+
+static void connectCbAssertOk(struct raft_uv_connect *req,
+                              struct uv_stream_s *stream,
+                              int status)
+{
+    bool *done = req->data;
+    munit_assert_int(status, ==, 0);
+    munit_assert_int(UV_TCP, ==, stream->type);
+    uv_close((struct uv_handle_s *)stream, (uv_close_cb)raft_free);
+    *done = true;
 }
 
-#define CONNECT(RV)                                                   \
+static void connectCbAssertFail(struct raft_uv_connect *req,
+                                struct uv_stream_s *stream,
+                                int status)
+{
+    struct connectResult *result = req->data;
+    (void)stream;
+    munit_assert_int(status, !=, 0);
+    munit_assert_int(status, ==, result->status);
+    /*munit_assert_string_equal(req->errmsg, result->errmsg);*/
+    result->done = true;
+}
+
+/* Submit a connect request with the given parameters and wait for the operation
+ * to successfully complete. */
+#define CONNECT(ID, ADDRESS)                                          \
     {                                                                 \
-        int rv;                                                       \
-        rv = f->transport.connect(&f->transport, &f->req, 2,          \
-                                  f->tcp.server.address, connect_cb); \
-        munit_assert_int(rv, ==, RV);                                 \
+        struct raft_uv_connect _req;                                  \
+        bool _done = false;                                           \
+        int _i;                                                       \
+        int _rv;                                                      \
+        _req.data = &_done;                                           \
+        _rv = f->transport.connect(&f->transport, &_req, ID, ADDRESS, \
+                                   connectCbAssertOk);                \
+        munit_assert_int(_rv, ==, 0);                                 \
+        for (_i = 0; _i < 2; _i++) {                                  \
+            LOOP_RUN(1);                                              \
+            if (_done) {                                              \
+                break;                                                \
+            }                                                         \
+        }                                                             \
+        munit_assert_true(_done);                                     \
     }
 
-#define WAIT_CONNECT_CB(STATUS)                  \
-    {                                            \
-        int i;                                   \
-        for (i = 0; i < 2; i++) {                \
-            if (f->invoked == 1)                 \
-                break;                           \
-            uv_run(&f->loop, UV_RUN_NOWAIT);     \
-        }                                        \
-        munit_assert_int(f->invoked, ==, 1);     \
-        munit_assert_int(f->status, ==, STATUS); \
+/* Submit a connect request and assert that it fails synchronously with the
+ * given error code and message. */
+#define CONNECT_ERROR(ID, ADDRESS, RV, ERRMSG)                               \
+    {                                                                        \
+        struct raft_uv_connect _req;                                         \
+        int _rv;                                                             \
+        _rv = f->transport.connect(&f->transport, &_req, ID, ADDRESS, NULL); \
+        munit_assert_int(_rv, ==, RV);                                       \
     }
 
-#define PEER_SHUTDOWN test_tcp_stop(&f->tcp);
+/* Submit a connect request with the given parameters and wait for the operation
+ * to fail with the given code and message. */
+#define CONNECT_FAILURE(ID, ADDRESS, STATUS, ERRMSG)                  \
+    {                                                                 \
+        struct raft_uv_connect _req;                                  \
+        struct connectResult _result = {STATUS, ERRMSG, false};       \
+        int _i;                                                       \
+        int _rv;                                                      \
+        _req.data = &_result;                                         \
+        _rv = f->transport.connect(&f->transport, &_req, ID, ADDRESS, \
+                                   connectCbAssertFail);              \
+        munit_assert_int(_rv, ==, 0);                                 \
+        for (_i = 0; _i < 2; _i++) {                                  \
+            LOOP_RUN(1);                                              \
+            if (_result.done) {                                       \
+                break;                                                \
+            }                                                         \
+        }                                                             \
+        munit_assert_true(_result.done);                              \
+    }
 
-#define CLOSE                               \
-    f->transport.stop(&f->transport);       \
-    raft_uv_tcp_close(&f->transport, NULL); \
-    f->closed = true;
+/* Submit a connect request with the given parameters, close the transport after
+ * N loop iterations and assert that the request got canceled.. */
+#define CONNECT_CANCEL(ID, ADDRESS, N)                                \
+    {                                                                 \
+        struct raft_uv_connect _req;                                  \
+        struct connectResult _result = {RAFT_CANCELED, "", false};    \
+        int _i;                                                       \
+        int _rv;                                                      \
+        _req.data = &_result;                                         \
+        _rv = f->transport.connect(&f->transport, &_req, ID, ADDRESS, \
+                                   connectCbAssertFail);              \
+        munit_assert_int(_rv, ==, 0);                                 \
+        for (_i = 0; _i < N; _i++) {                                  \
+            LOOP_RUN(1);                                              \
+            munit_assert_false(_result.done);                         \
+        }                                                             \
+        f->transport.stop(&f->transport);                             \
+        raft_uv_tcp_close(&f->transport, NULL);                       \
+        f->closed = true;                                             \
+        for (_i = 0; _i < 2; _i++) {                                  \
+            LOOP_RUN(1);                                              \
+            if (_result.done) {                                       \
+                break;                                                \
+            }                                                         \
+        }                                                             \
+        munit_assert_true(_result.done);                              \
+    }
 
 /******************************************************************************
  *
@@ -108,87 +173,71 @@ static void connect_cb(struct raft_uv_connect *req,
 SUITE(tcp_connect)
 
 /* Successfully connect to the peer. */
-TEST(tcp_connect, first, setup, tear_down, 0, NULL)
+TEST(tcp_connect, first, setUp, tearDown, 0, NULL)
 {
     struct fixture *f = data;
-    (void)params;
-    CONNECT(0);
-    WAIT_CONNECT_CB(0);
-    munit_assert_ptr_not_null(f->stream);
-    uv_close((struct uv_handle_s *)f->stream, (uv_close_cb)raft_free);
+    CONNECT(2, f->tcp.server.address);
     return MUNIT_OK;
 }
 
 /* The peer has shutdown */
-TEST(tcp_connect, refused, setup, tear_down, 0, NULL)
+TEST(tcp_connect, refused, setUp, tearDown, 0, NULL)
 {
     struct fixture *f = data;
     (void)params;
-    PEER_SHUTDOWN;
-    CONNECT(0);
-    WAIT_CONNECT_CB(RAFT_NOCONNECTION);
+    CONNECT_FAILURE(2, "127.0.0.1:6666", RAFT_NOCONNECTION, "");
     return MUNIT_OK;
 }
 
-static char *connect_error_oom_heap_fault_delay[] = {"0", "1", NULL};
-static char *connect_error_oom_heap_fault_repeat[] = {"1", NULL};
+static char *oomHeapFaultDelay[] = {"0", "1", NULL};
+static char *oomHeapFaultRepeat[] = {"1", NULL};
 
-static MunitParameterEnum connect_error_oom_params[] = {
-    {TEST_HEAP_FAULT_DELAY, connect_error_oom_heap_fault_delay},
-    {TEST_HEAP_FAULT_REPEAT, connect_error_oom_heap_fault_repeat},
+static MunitParameterEnum oomParams[] = {
+    {TEST_HEAP_FAULT_DELAY, oomHeapFaultDelay},
+    {TEST_HEAP_FAULT_REPEAT, oomHeapFaultRepeat},
     {NULL, NULL},
 };
 
 /* Out of memory conditions. */
-TEST(tcp_connect, oom, setup, tear_down, 0, connect_error_oom_params)
+TEST(tcp_connect, oom, setUp, tearDown, 0, oomParams)
 {
     struct fixture *f = data;
-    (void)params;
     test_heap_fault_enable(&f->heap);
-    CONNECT(RAFT_NOMEM);
+    CONNECT_ERROR(2, f->tcp.server.address, RAFT_NOMEM, "");
     return MUNIT_OK;
 }
 
-static char *connect_error_oom_async_heap_fault_delay[] = {"0", NULL};
-static char *connect_error_oom_async_heap_fault_repeat[] = {"1", NULL};
+static char *oomAsyncHeapFaultDelay[] = {"3", NULL};
+static char *oomAsyncHeapFaultRepeat[] = {"1", NULL};
 
-static MunitParameterEnum connect_error_oom_async_params[] = {
-    {TEST_HEAP_FAULT_DELAY, connect_error_oom_async_heap_fault_delay},
-    {TEST_HEAP_FAULT_REPEAT, connect_error_oom_async_heap_fault_repeat},
+static MunitParameterEnum oomAsyncParams[] = {
+    {TEST_HEAP_FAULT_DELAY, oomAsyncHeapFaultDelay},
+    {TEST_HEAP_FAULT_REPEAT, oomAsyncHeapFaultRepeat},
     {NULL, NULL},
 };
 
 /* Out of memory condition after the attempt has started. */
-TEST(tcp_connect, oom_assync, setup, tear_down, 0, connect_error_oom_async_params)
+TEST(tcp_connect, oomAsync, setUp, tearDown, 0, oomAsyncParams)
 {
     struct fixture *f = data;
-    (void)params;
-    CONNECT(0);
     test_heap_fault_enable(&f->heap);
-    WAIT_CONNECT_CB(RAFT_NOMEM);
+    CONNECT_FAILURE(2, f->tcp.server.address, RAFT_NOMEM, "");
     return MUNIT_OK;
 }
 
 /* The transport is closed immediately after a connect request as been
  * submitted. The request's callback is invoked with RAFT_CANCELED. */
-TEST(tcp_connect, closeImmediately, setup, tear_down, 0, NULL)
+TEST(tcp_connect, closeImmediately, setUp, tearDown, 0, NULL)
 {
     struct fixture *f = data;
-    (void)params;
-    CONNECT(0);
-    CLOSE;
-    WAIT_CONNECT_CB(RAFT_CANCELED);
+    CONNECT_CANCEL(2, f->tcp.server.address, 0);
     return MUNIT_OK;
 }
 
 /* The transport gets closed during the handshake. */
-TEST(tcp_connect, closeDuringHandshake, setup, tear_down, 0, NULL)
+TEST(tcp_connect, closeDuringHandshake, setUp, tearDown, 0, NULL)
 {
     struct fixture *f = data;
-    (void)params;
-    CONNECT(0);
-    uv_run(&f->loop, UV_RUN_NOWAIT);
-    CLOSE;
-    WAIT_CONNECT_CB(RAFT_CANCELED);
+    CONNECT_CANCEL(2, f->tcp.server.address, 1);
     return MUNIT_OK;
 }
