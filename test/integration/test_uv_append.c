23ac99575dbfaaf4bbc994fa136de43beffed9fd
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-11-19 23:49:38 +0000

    Use io.load() to assert append() outcome

--- test/integration/test_uv_append.c
+++ test/integration/test_uv_append.c
@@ -18,7 +18,6 @@ struct fixture
 {
     FIXTURE_UV_DEPS;
     FIXTURE_UV;
-    struct raft_entry *entries;
     int count; /* To generate deterministic entry data */
 };
 
@@ -41,6 +40,9 @@ static void appendCbAssertResult(struct raft_io_append *req, int status)
     result->done = true;
 }
 
+/* Declare and fill the entries array for the append request identified by
+ * I. The array will have N entries, and each entry will have a data buffer of
+ * SIZE bytes.*/
 #define ENTRIES(I, N, SIZE)                                 \
     struct raft_entry _entries##I[N];                       \
     uint8_t _entries_data##I[N * SIZE];                     \
@@ -111,6 +113,12 @@ static void appendCbAssertResult(struct raft_io_append *req, int status)
         /*munit_assert_string_equal(f->transport.errmsg, ERRMSG);*/ \
     }
 
+/******************************************************************************
+ *
+ * Set up and tear down.
+ *
+ *****************************************************************************/
+
 static void *setUp(const MunitParameter params[], void *user_data)
 {
     struct fixture *f = munit_malloc(sizeof *f);
@@ -138,155 +146,68 @@ static void tearDown(void *data)
 
 /******************************************************************************
  *
- * Helper macros
- *
- *****************************************************************************/
-
-/* Taken from https://github.com/gcc-mirror/gcc/blob/master/libiberty/crc32.c */
-static const unsigned table[] = {
-    0x00000000, 0x04c11db7, 0x09823b6e, 0x0d4326d9, 0x130476dc, 0x17c56b6b,
-    0x1a864db2, 0x1e475005, 0x2608edb8, 0x22c9f00f, 0x2f8ad6d6, 0x2b4bcb61,
-    0x350c9b64, 0x31cd86d3, 0x3c8ea00a, 0x384fbdbd, 0x4c11db70, 0x48d0c6c7,
-    0x4593e01e, 0x4152fda9, 0x5f15adac, 0x5bd4b01b, 0x569796c2, 0x52568b75,
-    0x6a1936c8, 0x6ed82b7f, 0x639b0da6, 0x675a1011, 0x791d4014, 0x7ddc5da3,
-    0x709f7b7a, 0x745e66cd, 0x9823b6e0, 0x9ce2ab57, 0x91a18d8e, 0x95609039,
-    0x8b27c03c, 0x8fe6dd8b, 0x82a5fb52, 0x8664e6e5, 0xbe2b5b58, 0xbaea46ef,
-    0xb7a96036, 0xb3687d81, 0xad2f2d84, 0xa9ee3033, 0xa4ad16ea, 0xa06c0b5d,
-    0xd4326d90, 0xd0f37027, 0xddb056fe, 0xd9714b49, 0xc7361b4c, 0xc3f706fb,
-    0xceb42022, 0xca753d95, 0xf23a8028, 0xf6fb9d9f, 0xfbb8bb46, 0xff79a6f1,
-    0xe13ef6f4, 0xe5ffeb43, 0xe8bccd9a, 0xec7dd02d, 0x34867077, 0x30476dc0,
-    0x3d044b19, 0x39c556ae, 0x278206ab, 0x23431b1c, 0x2e003dc5, 0x2ac12072,
-    0x128e9dcf, 0x164f8078, 0x1b0ca6a1, 0x1fcdbb16, 0x018aeb13, 0x054bf6a4,
-    0x0808d07d, 0x0cc9cdca, 0x7897ab07, 0x7c56b6b0, 0x71159069, 0x75d48dde,
-    0x6b93dddb, 0x6f52c06c, 0x6211e6b5, 0x66d0fb02, 0x5e9f46bf, 0x5a5e5b08,
-    0x571d7dd1, 0x53dc6066, 0x4d9b3063, 0x495a2dd4, 0x44190b0d, 0x40d816ba,
-    0xaca5c697, 0xa864db20, 0xa527fdf9, 0xa1e6e04e, 0xbfa1b04b, 0xbb60adfc,
-    0xb6238b25, 0xb2e29692, 0x8aad2b2f, 0x8e6c3698, 0x832f1041, 0x87ee0df6,
-    0x99a95df3, 0x9d684044, 0x902b669d, 0x94ea7b2a, 0xe0b41de7, 0xe4750050,
-    0xe9362689, 0xedf73b3e, 0xf3b06b3b, 0xf771768c, 0xfa325055, 0xfef34de2,
-    0xc6bcf05f, 0xc27dede8, 0xcf3ecb31, 0xcbffd686, 0xd5b88683, 0xd1799b34,
-    0xdc3abded, 0xd8fba05a, 0x690ce0ee, 0x6dcdfd59, 0x608edb80, 0x644fc637,
-    0x7a089632, 0x7ec98b85, 0x738aad5c, 0x774bb0eb, 0x4f040d56, 0x4bc510e1,
-    0x46863638, 0x42472b8f, 0x5c007b8a, 0x58c1663d, 0x558240e4, 0x51435d53,
-    0x251d3b9e, 0x21dc2629, 0x2c9f00f0, 0x285e1d47, 0x36194d42, 0x32d850f5,
-    0x3f9b762c, 0x3b5a6b9b, 0x0315d626, 0x07d4cb91, 0x0a97ed48, 0x0e56f0ff,
-    0x1011a0fa, 0x14d0bd4d, 0x19939b94, 0x1d528623, 0xf12f560e, 0xf5ee4bb9,
-    0xf8ad6d60, 0xfc6c70d7, 0xe22b20d2, 0xe6ea3d65, 0xeba91bbc, 0xef68060b,
-    0xd727bbb6, 0xd3e6a601, 0xdea580d8, 0xda649d6f, 0xc423cd6a, 0xc0e2d0dd,
-    0xcda1f604, 0xc960ebb3, 0xbd3e8d7e, 0xb9ff90c9, 0xb4bcb610, 0xb07daba7,
-    0xae3afba2, 0xaafbe615, 0xa7b8c0cc, 0xa379dd7b, 0x9b3660c6, 0x9ff77d71,
-    0x92b45ba8, 0x9675461f, 0x8832161a, 0x8cf30bad, 0x81b02d74, 0x857130c3,
-    0x5d8a9099, 0x594b8d2e, 0x5408abf7, 0x50c9b640, 0x4e8ee645, 0x4a4ffbf2,
-    0x470cdd2b, 0x43cdc09c, 0x7b827d21, 0x7f436096, 0x7200464f, 0x76c15bf8,
-    0x68860bfd, 0x6c47164a, 0x61043093, 0x65c52d24, 0x119b4be9, 0x155a565e,
-    0x18197087, 0x1cd86d30, 0x029f3d35, 0x065e2082, 0x0b1d065b, 0x0fdc1bec,
-    0x3793a651, 0x3352bbe6, 0x3e119d3f, 0x3ad08088, 0x2497d08d, 0x2056cd3a,
-    0x2d15ebe3, 0x29d4f654, 0xc5a92679, 0xc1683bce, 0xcc2b1d17, 0xc8ea00a0,
-    0xd6ad50a5, 0xd26c4d12, 0xdf2f6bcb, 0xdbee767c, 0xe3a1cbc1, 0xe760d676,
-    0xea23f0af, 0xeee2ed18, 0xf0a5bd1d, 0xf464a0aa, 0xf9278673, 0xfde69bc4,
-    0x89b8fd09, 0x8d79e0be, 0x803ac667, 0x84fbdbd0, 0x9abc8bd5, 0x9e7d9662,
-    0x933eb0bb, 0x97ffad0c, 0xafb010b1, 0xab710d06, 0xa6322bdf, 0xa2f33668,
-    0xbcb4666d, 0xb8757bda, 0xb5365d03, 0xb1f740b4};
-
-unsigned byteCrc32(const void *buf, const size_t size, const unsigned init)
-{
-    unsigned crc = init;
-    uint8_t *cursor = (uint8_t *)buf;
-    size_t count = size;
-
-    while (count--) {
-        crc = (crc << 8) ^ table[((crc >> 24) ^ *cursor) & 255];
-        cursor++;
-    }
-    return crc;
-}
-
-/******************************************************************************
- *
  * Assertions
  *
  *****************************************************************************/
 
-/* Assert that the open segment with the given counter has format version 1 and
- * N entries with a total data size of S bytes. */
-#define ASSERT_SEGMENT(COUNTER, N, SIZE)                                     \
-    {                                                                        \
-        struct raft_buffer buf;                                              \
-        const void *cursor;                                                  \
-        char filename[strlen("open-N") + 1];                                 \
-        unsigned i_ = 0;                                                     \
-        size_t total_data_size = 0;                                          \
-                                                                             \
-        sprintf(filename, "open-%d", COUNTER);                               \
-                                                                             \
-        buf.len = MAX_SEGMENT_BLOCKS * SEGMENT_BLOCK_SIZE;                   \
-        if (buf.len < 8 + /* segment header */ +SIZE) {                      \
-            size_t rest;                                                     \
-            buf.len = 8 /* segment header */ + SIZE;                         \
-            rest = buf.len % SEGMENT_BLOCK_SIZE;                             \
-            if (rest != 0) {                                                 \
-                buf.len += SEGMENT_BLOCK_SIZE - rest;                        \
-            }                                                                \
-        }                                                                    \
-        buf.base = munit_malloc(buf.len);                                    \
-                                                                             \
-        test_dir_read_file(f->dir, filename, buf.base, buf.len);             \
-                                                                             \
-        cursor = buf.base;                                                   \
-        munit_assert_int(byteGet64(&cursor), ==, 1);                         \
-                                                                             \
-        while (i_ < N) {                                                     \
-            unsigned crc1 = byteGet32(&cursor);                              \
-            unsigned crc2 = byteGet32(&cursor);                              \
-            const void *header = cursor;                                     \
-            const void *content;                                             \
-            unsigned n_ = byteGet64(&cursor);                                \
-            struct raft_entry *entries = munit_malloc(n_ * sizeof *entries); \
-            unsigned j_;                                                     \
-            unsigned crc;                                                    \
-            size_t data_size = 0;                                            \
-                                                                             \
-            for (j_ = 0; j_ < n_; j_++) {                                    \
-                struct raft_entry *entry = &entries[j_];                     \
-                                                                             \
-                entry->term = byteGet64(&cursor);                            \
-                entry->type = byteGet8(&cursor);                             \
-                byteGet8(&cursor);                                           \
-                byteGet8(&cursor);                                           \
-                byteGet8(&cursor);                                           \
-                entry->buf.len = byteGet32(&cursor);                         \
-                                                                             \
-                munit_assert_int(entry->term, ==, 1);                        \
-                munit_assert_int(entry->type, ==, RAFT_COMMAND);             \
-                                                                             \
-                data_size += entry->buf.len;                                 \
-            }                                                                \
-                                                                             \
-            crc = byteCrc32(header, 8 + 16 * n_, 0);                         \
-            munit_assert_int(crc, ==, crc1);                                 \
-                                                                             \
-            content = cursor;                                                \
-                                                                             \
-            for (j_ = 0; j_ < n_; j_++) {                                    \
-                struct raft_entry *entry = &entries[j_];                     \
-                uint64_t value;                                              \
-                value = byteFlip64(*(uint64_t *)cursor);                     \
-                munit_assert_int(value, ==, i_);                             \
-                cursor = (uint8_t *)cursor + entry->buf.len;                 \
-                i_++;                                                        \
-            }                                                                \
-                                                                             \
-            crc = byteCrc32(content, data_size, 0);                          \
-            munit_assert_int(crc, ==, crc2);                                 \
-                                                                             \
-            free(entries);                                                   \
-                                                                             \
-            total_data_size += data_size;                                    \
-        }                                                                    \
-                                                                             \
-        munit_assert_int(total_data_size, ==, SIZE);                         \
-        free(buf.base);                                                      \
-    }
+/* Shutdown the fixture's raft_io instance, then load all entries on disk using
+ * a new raft_io instance, and assert that there are N entries with a total data
+ * size of TOTAL_DATA_SIZE bytes. */
+#define ASSERT_ENTRIES(N, TOTAL_DATA_SIZE)                               \
+    TEAR_DOWN_UV;                                                        \
+    do {                                                                 \
+        struct uv_loop_s _loop;                                          \
+        struct raft_uv_transport _transport;                             \
+        struct raft_io _io;                                              \
+        raft_term _term;                                                 \
+        unsigned _voted_for;                                             \
+        struct raft_snapshot *_snapshot;                                 \
+        raft_index _start_index;                                         \
+        struct raft_entry *_entries;                                     \
+        size_t _i;                                                       \
+        size_t _n;                                                       \
+        void *_batch = NULL;                                             \
+        size_t _total_data_size = 0;                                     \
+        int _rv;                                                         \
+                                                                         \
+        _rv = uv_loop_init(&_loop);                                      \
+        munit_assert_int(_rv, ==, 0);                                    \
+        _rv = raft_uv_tcp_init(&_transport, &_loop);                     \
+        munit_assert_int(_rv, ==, 0);                                    \
+        _rv = raft_uv_init(&_io, &_loop, f->dir, &_transport);           \
+        munit_assert_int(_rv, ==, 0);                                    \
+        _rv = _io.init(&_io, 1, "1");                                    \
+        munit_assert_int(_rv, ==, 0);                                    \
+        _rv = _io.load(&_io, 10, &_term, &_voted_for, &_snapshot,        \
+                       &_start_index, &_entries, &_n);                   \
+        munit_assert_int(_rv, ==, 0);                                    \
+        _io.close(&_io, NULL);                                           \
+        uv_run(&_loop, UV_RUN_NOWAIT);                                   \
+        raft_uv_close(&_io);                                             \
+        raft_uv_tcp_close(&_transport);                                  \
+        uv_loop_close(&_loop);                                           \
+                                                                         \
+        munit_assert_ptr_null(_snapshot);                                \
+        munit_assert_int(_n, ==, N);                                     \
+        for (_i = 0; _i < _n; _i++) {                                    \
+            struct raft_entry *_entry = &_entries[_i];                   \
+            uint64_t _value = byteFlip64(*(uint64_t *)_entry->buf.base); \
+            munit_assert_int(_entry->term, ==, 1);                       \
+            munit_assert_int(_entry->type, ==, RAFT_COMMAND);            \
+            munit_assert_int(_value, ==, _i);                            \
+            munit_assert_ptr_not_null(_entry->batch);                    \
+        }                                                                \
+        for (_i = 0; _i < _n; _i++) {                                    \
+            struct raft_entry *_entry = &_entries[_i];                   \
+            if (_entry->batch != _batch) {                               \
+                _batch = _entry->batch;                                  \
+                raft_free(_batch);                                       \
+            }                                                            \
+            _total_data_size += _entry->buf.len;                         \
+        }                                                                \
+        raft_free(_entries);                                             \
+        munit_assert_int(_total_data_size, ==, TOTAL_DATA_SIZE);         \
+    } while (0);
 
 /******************************************************************************
  *
@@ -297,11 +218,11 @@ unsigned byteCrc32(const void *buf, const size_t size, const unsigned init)
 SUITE(append)
 
 /* Append the very first batch of entries. */
-TEST(append, first, setUp, tearDown, 0, NULL)
+TEST(append, first, setUp, tearDownDeps, 0, NULL)
 {
     struct fixture *f = data;
     APPEND(1, 64);
-    ASSERT_SEGMENT(1, 1, 64);
+    ASSERT_ENTRIES(1, 64);
     return MUNIT_OK;
 }
 
@@ -339,13 +260,11 @@ TEST(append, finalizeSegment, setUp, tearDown, 0, NULL)
 
 /* The very first batch of entries to append is bigger than the regular open
  * segment size. */
-TEST(append, firstBig, setUp, tearDown, 0, NULL)
+TEST(append, firstBig, setUp, tearDownDeps, 0, NULL)
 {
     struct fixture *f = data;
-    /* struct uv *uv = f->io.impl; */
     APPEND(MAX_SEGMENT_BLOCKS, SEGMENT_BLOCK_SIZE);
-    ASSERT_SEGMENT(1, MAX_SEGMENT_BLOCKS,
-                   MAX_SEGMENT_BLOCKS * SEGMENT_BLOCK_SIZE);
+    ASSERT_ENTRIES(MAX_SEGMENT_BLOCKS, MAX_SEGMENT_BLOCKS * SEGMENT_BLOCK_SIZE);
     return MUNIT_OK;
 }
 
@@ -361,17 +280,17 @@ TEST(append, secondBig, setUp, tearDown, 0, NULL)
 
 /* Write the very first entry and then another one, both fitting in the same
  * block. */
-TEST(append, fitBlock, setUp, tearDown, 0, NULL)
+TEST(append, fitBlock, setUp, tearDownDeps, 0, NULL)
 {
     struct fixture *f = data;
     APPEND(1, 64);
     APPEND(1, 64);
-    ASSERT_SEGMENT(1, 2, 128);
+    ASSERT_ENTRIES(2, 128);
     return MUNIT_OK;
 }
 
 /* Write an entry that fills the first block exactly and then another one. */
-TEST(append, matchBlock, setUp, tearDown, 0, NULL)
+TEST(append, matchBlock, setUp, tearDownDeps, 0, NULL)
 {
     struct fixture *f = data;
     size_t size;
@@ -384,7 +303,7 @@ TEST(append, matchBlock, setUp, tearDown, 0, NULL)
     APPEND(1, size);
     APPEND(1, 64);
 
-    ASSERT_SEGMENT(1, 2, size + 64);
+    ASSERT_ENTRIES(2, size + 64);
 
     return MUNIT_OK;
 }
@@ -393,7 +312,7 @@ TEST(append, matchBlock, setUp, tearDown, 0, NULL)
  * the second block, then a third one that fills the rest of the second block
  * plus the whole third block exactly, and finally a fourth entry that fits in
  * the fourth block */
-TEST(append, exceedBlock, setUp, tearDown, 0, NULL)
+TEST(append, exceedBlock, setUp, tearDownDeps, 0, NULL)
 {
     struct fixture *f = data;
     size_t written;
@@ -423,7 +342,7 @@ TEST(append, exceedBlock, setUp, tearDown, 0, NULL)
     /* Write a fourth entry */
     APPEND(1, 64);
 
-    ASSERT_SEGMENT(1, 4, size1 + 64 + size2 + 64);
+    ASSERT_ENTRIES(4, size1 + 64 + size2 + 64);
 
     return MUNIT_OK;
 }
@@ -456,7 +375,7 @@ TEST(append, wait, setUp, tearDown, 0, NULL)
 
 /* Several batches with different size gets appended in fast pace, forcing the
  * segment arena to grow. */
-TEST(append, resizeArena, setUp, tearDown, 0, NULL)
+TEST(append, resizeArena, setUp, tearDownDeps, 0, NULL)
 {
     struct fixture *f = data;
     APPEND_SUBMIT(0, 2, 64);
@@ -469,7 +388,7 @@ TEST(append, resizeArena, setUp, tearDown, 0, NULL)
     APPEND_WAIT(2);
     APPEND_WAIT(3);
     APPEND_WAIT(4);
-    ASSERT_SEGMENT(1, 7, 64 * 4 + SEGMENT_BLOCK_SIZE * 3);
+    ASSERT_ENTRIES(7, 64 * 4 + SEGMENT_BLOCK_SIZE * 3);
     return MUNIT_OK;
 }
 
