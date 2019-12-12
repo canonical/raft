901fa4ee66594ac0eac6274d770e63d9575b6d20
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-12-12 13:20:12 +0000

    Clean up test names in test_replication.c

--- test/integration/test_replication.c
+++ test/integration/test_replication.c
@@ -74,14 +74,14 @@ static MunitParameterEnum cluster_3_params[] = {
 
 /******************************************************************************
  *
- * Send AppendEntries messages
+ * Log replication.
  *
  *****************************************************************************/
 
-SUITE(send)
+SUITE(replication)
 
 /* A leader sends a heartbeat message as soon as it gets elected. */
-TEST(send, initialHeartbeat, setup, tear_down, 0, NULL)
+TEST(replication, sendInitialHeartbeat, setup, tear_down, 0, NULL)
 {
     struct fixture *f = data;
     struct raft *raft;
@@ -117,7 +117,7 @@ TEST(send, initialHeartbeat, setup, tear_down, 0, NULL)
 
 /* A leader keeps sending heartbeat messages at regular intervals to
  * maintain leadership. */
-TEST(send, followupHeartbeat, setup, tear_down, 0, NULL)
+TEST(replication, sendFollowupHeartbeat, setup, tear_down, 0, NULL)
 {
     struct fixture *f = data;
     struct raft *raft;
@@ -157,7 +157,7 @@ TEST(send, followupHeartbeat, setup, tear_down, 0, NULL)
 
 /* If a leader replicates some entries during a given heartbeat interval, it
  * skips sending the heartbeat for that interval. */
-TEST(send, skipHeartbeat, setup, tear_down, 0, NULL)
+TEST(replication, sendSkipHeartbeat, setup, tear_down, 0, NULL)
 {
     struct fixture *f = data;
     struct raft *raft;
@@ -194,7 +194,7 @@ TEST(send, skipHeartbeat, setup, tear_down, 0, NULL)
 
 /* A follower remains in probe mode until the leader receives a successful
  * AppendEntries response. */
-TEST(send, probe, setup, tear_down, 0, NULL)
+TEST(replication, sendProbe, setup, tear_down, 0, NULL)
 {
     struct fixture *f = data;
     struct raft_apply req1;
@@ -243,7 +243,7 @@ TEST(send, probe, setup, tear_down, 0, NULL)
 
 /* A follower transitions to pipeline mode after the leader receives a
  * successful AppendEntries response from it. */
-TEST(send, pipeline, setup, tear_down, 0, NULL)
+TEST(replication, sendPipeline, setup, tear_down, 0, NULL)
 {
     struct fixture *f = data;
     struct raft *raft;
@@ -285,7 +285,7 @@ TEST(send, pipeline, setup, tear_down, 0, NULL)
 }
 
 /* A follower disconnects while in probe mode. */
-TEST(send, disconnect, setup, tear_down, 0, NULL)
+TEST(replication, sendDisconnect, setup, tear_down, 0, NULL)
 {
     struct fixture *f = data;
     CLUSTER_BOOTSTRAP;
@@ -315,7 +315,7 @@ TEST(send, disconnect, setup, tear_down, 0, NULL)
 }
 
 /* A follower disconnects while in pipeline mode. */
-TEST(send, disconnectPipeline, setup, tear_down, 0, NULL)
+TEST(replication, sendDisconnectPipeline, setup, tear_down, 0, NULL)
 {
     struct fixture *f = data;
     struct raft_apply req1;
@@ -362,7 +362,7 @@ static MunitParameterEnum send_oom_params[] = {
 };
 
 /* Out of memory failures. */
-TEST(send, oom, setup, tear_down, 0, send_oom_params)
+TEST(replication, sendOom, setup, tear_down, 0, send_oom_params)
 {
     struct fixture *f = data;
     return MUNIT_SKIP;
@@ -378,7 +378,7 @@ TEST(send, oom, setup, tear_down, 0, send_oom_params)
 }
 
 /* A failure occurs upon submitting the I/O request. */
-TEST(send, ioError, setup, tear_down, 0, NULL)
+TEST(replication, sendIoError, setup, tear_down, 0, NULL)
 {
     struct fixture *f = data;
     return MUNIT_SKIP;
@@ -393,16 +393,8 @@ TEST(send, ioError, setup, tear_down, 0, NULL)
     return MUNIT_OK;
 }
 
-/******************************************************************************
- *
- * Receive AppendEntries requests
- *
- *****************************************************************************/
-
-SUITE(receive)
-
 /* Receive the same entry a second time, before the first has been persisted. */
-TEST(receive, twice, setup, tear_down, 0, NULL)
+TEST(replication, recvTwice, setup, tear_down, 0, NULL)
 {
     struct fixture *f = data;
     struct raft_apply *req = munit_malloc(sizeof *req);
@@ -427,7 +419,7 @@ TEST(receive, twice, setup, tear_down, 0, NULL)
 }
 
 /* If the term in the request is stale, the server rejects it. */
-TEST(receive, stale_term, setup, tear_down, 0, cluster_3_params)
+TEST(replication, recvStaleTerm, setup, tear_down, 0, cluster_3_params)
 {
     struct fixture *f = data;
     BOOTSTRAP_START_AND_ELECT;
@@ -455,7 +447,7 @@ TEST(receive, stale_term, setup, tear_down, 0, cluster_3_params)
 }
 
 /* If server's log is shorter than prevLogIndex, the request is rejected . */
-TEST(receive, missing_entries, setup, tear_down, 0, NULL)
+TEST(replication, recvMissingEntries, setup, tear_down, 0, NULL)
 {
     struct fixture *f = data;
     struct raft_entry entry;
@@ -481,7 +473,7 @@ TEST(receive, missing_entries, setup, tear_down, 0, NULL)
 /* If the term of the last log entry on the server is different from the one
  * prevLogTerm, and value of prevLogIndex is greater than server's commit commit
  * index (i.e. this is a normal inconsistency), we reject the request. */
-TEST(receive, prev_log_term_mismatch, setup, tear_down, 0, NULL)
+TEST(replication, recvPrevLogTermMismatch, setup, tear_down, 0, NULL)
 {
     struct fixture *f = data;
     struct raft_entry entry1;
@@ -511,7 +503,7 @@ TEST(receive, prev_log_term_mismatch, setup, tear_down, 0, NULL)
 /* If any of the new entry has the same index of an existing entry in our log,
  * but different term, and that entry index is already committed, we bail out
  * with an error. */
-TEST(receive, prev_index_conflict, setup, tear_down, 0, NULL)
+TEST(replication, recvPrevIndexConflict, setup, tear_down, 0, NULL)
 {
     struct fixture *f = data;
     struct raft_entry entry1;
@@ -542,7 +534,7 @@ TEST(receive, prev_index_conflict, setup, tear_down, 0, NULL)
 
 /* A write log request is submitted for outstanding log entries. If some entries
  * are already existing in the log, they will be skipped. */
-TEST(receive, skip, setup, tear_down, 0, NULL)
+TEST(replication, recvSkip, setup, tear_down, 0, NULL)
 {
     struct fixture *f = data;
     struct raft_apply *req = munit_malloc(sizeof *req);
@@ -569,7 +561,7 @@ TEST(receive, skip, setup, tear_down, 0, NULL)
 
 /* If the index and term of the last snapshot on the server match prevLogIndex
  * and prevLogTerm the request is accepted. */
-TEST(receive, match_last_snapshot, setup, tear_down, 0, NULL)
+TEST(replication, recvMatch_last_snapshot, setup, tear_down, 0, NULL)
 {
     struct fixture *f = data;
     struct raft_entry entry;
@@ -608,7 +600,7 @@ TEST(receive, match_last_snapshot, setup, tear_down, 0, NULL)
 }
 /* If a candidate server receives a request contaning the same term as its
  * own, it it steps down to follower and accept the request . */
-TEST(receive, candidateSameTerm, setup, tear_down, 0, cluster_3_params)
+TEST(replication, recvCandidateSameTerm, setup, tear_down, 0, cluster_3_params)
 {
     struct fixture *f = data;
     CLUSTER_BOOTSTRAP;
@@ -646,7 +638,7 @@ TEST(receive, candidateSameTerm, setup, tear_down, 0, cluster_3_params)
 
 /* If a candidate server receives a request contaning an higher term as its
  * own, it it steps down to follower and accept the request . */
-TEST(receive, candidateHigherTerm, setup, tear_down, 0, cluster_3_params)
+TEST(replication, recvCandidateHigherTerm, setup, tear_down, 0, cluster_3_params)
 {
     struct fixture *f = data;
     CLUSTER_BOOTSTRAP;
@@ -697,17 +689,9 @@ TEST(receive, candidateHigherTerm, setup, tear_down, 0, cluster_3_params)
     return MUNIT_OK;
 }
 
-/******************************************************************************
- *
- * Receive AppendEntries responses
- *
- *****************************************************************************/
-
-SUITE(result)
-
 /* If the server handling the response is not the leader, the result
  * is ignored. */
-TEST(result, notLeader, setup, tear_down, 0, NULL)
+TEST(replication, resultNotLeader, setup, tear_down, 0, NULL)
 {
     struct fixture *f = data;
     BOOTSTRAP_START_AND_ELECT;
@@ -733,7 +717,7 @@ TEST(result, notLeader, setup, tear_down, 0, NULL)
 
 /* If the response has a term which is lower than the server's one, it's
  * ignored. */
-TEST(result, lowerTerm, setup, tear_down, 0, cluster_3_params)
+TEST(replication, resultLowerTerm, setup, tear_down, 0, cluster_3_params)
 {
     struct fixture *f = data;
     BOOTSTRAP_START_AND_ELECT;
@@ -765,7 +749,7 @@ TEST(result, lowerTerm, setup, tear_down, 0, cluster_3_params)
 
 /* If the response has a term which is higher than the server's one, step down
  * to follower. */
-TEST(result, higherTerm, setup, tear_down, 0, cluster_3_params)
+TEST(replication, resultHigherTerm, setup, tear_down, 0, cluster_3_params)
 {
     struct fixture *f = data;
     BOOTSTRAP_START_AND_ELECT;
@@ -793,7 +777,7 @@ TEST(result, higherTerm, setup, tear_down, 0, cluster_3_params)
 
 /* If the response fails because a log mismatch, the nextIndex for the server is
  * updated and the relevant older entries are resent. */
-TEST(result, retry, setup, tear_down, 0, NULL)
+TEST(replication, resultRetry, setup, tear_down, 0, NULL)
 {
     struct fixture *f = data;
     struct raft_entry entry;
