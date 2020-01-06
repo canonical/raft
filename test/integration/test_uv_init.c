8178a185769c541063a735d6d4aeff17d4d275d6
Free Ekanayaka <free.ekanayaka@canonical.com>
2020-01-06 10:13:08 +0000

    Skip failing test on ppc64el

--- test/integration/test_uv_init.c
+++ test/integration/test_uv_init.c
@@ -148,6 +148,10 @@ TEST(init, oom, setUp, tearDown, 0, oomParams)
     /* XXX: tmpfs seems to not support O_DIRECT */
     return MUNIT_SKIP;
 #endif
+#if defined(__powerpc64__)
+    /* XXX: fails on ppc64el */
+    return MUNIT_SKIP;
+#endif
     HEAP_FAULT_ENABLE;
     INIT_ERROR(f->dir, RAFT_NOMEM, "out of memory");
     return 0;
