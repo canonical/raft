b27693f174e7c3d6a8218549cced368739ea116f
Free Ekanayaka <free.ekanayaka@canonical.com>
2020-04-18 11:47:26 +0100

    Only include AIO syscall wrappers on Linux
    Fixes #125.

--- src/syscall.h
+++ src/syscall.h
@@ -3,13 +3,17 @@
 #ifndef SYSCALL_H_
 #define SYSCALL_H_
 
+#if HAVE_LINUX_AIO_ABI_H
 #include <linux/aio_abi.h>
 #include <signal.h>
 #include <time.h>
+#endif
+
 #if HAVE_LINUX_IO_URING_H
 #include <linux/io_uring.h>
 #endif
 
+#if HAVE_LINUX_AIO_ABI_H
 /* AIO */
 int io_setup(unsigned nr_events, aio_context_t *ctx_idp);
 
@@ -22,6 +26,7 @@ int io_getevents(aio_context_t ctx_id,
                  long nr,
                  struct io_event *events,
                  struct timespec *timeout);
+#endif
 
 #if HAVE_LINUX_IO_URING_H
 /* uring */
