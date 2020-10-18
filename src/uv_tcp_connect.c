e6aef84f903b5dfd81185ae2dd9cee8939d4180b
Josh Soref <jsoref@users.noreply.github.com>
2020-10-17 22:40:34 -0400

    spelling: connection
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/uv_tcp_connect.c
+++ src/uv_tcp_connect.c
@@ -29,7 +29,7 @@ struct uvTcpConnect
     struct raft_uv_connect *req; /* User request */
     uv_buf_t handshake;          /* Handshake data */
     struct uv_tcp_s *tcp;        /* TCP connection socket handle */
-    struct uv_connect_s connect; /* TCP connectionr request */
+    struct uv_connect_s connect; /* TCP connection request */
     struct uv_write_s write;     /* TCP handshake request */
     int status;                  /* Returned to the request callback */
     queue queue;                 /* Pending connect queue */
