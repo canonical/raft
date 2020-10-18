3079b6fa439f7956d23de287b3559c1b21b691d8
Josh Soref <jsoref@users.noreply.github.com>
2020-10-17 22:41:13 -0400

    spelling: requirement
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/uv_segment.c
+++ src/uv_segment.c
@@ -737,7 +737,7 @@ int uvSegmentBufferAppend(struct uvSegmentBuffer *b,
     crc2 = 0;
     for (i = 0; i < n_entries; i++) {
         const struct raft_entry *entry = &entries[i];
-        /* TODO: enforce the requirment of 8-byte alignment also in the
+        /* TODO: enforce the requirement of 8-byte alignment also in the
          * higher-level APIs. */
         assert(entry->buf.len % sizeof(uint64_t) == 0);
         memcpy(cursor, entry->buf.base, entry->buf.len);
