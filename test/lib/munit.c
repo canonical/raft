0b710ff0fc13f300699a080f98b3bae652d763f6
Mathieu Borderé <mathieu.bordere@canonical.com>
2021-03-25 15:27:54 +0100

    munit: call __gcov_flush when compiling with coverage

--- test/lib/munit.c
+++ test/lib/munit.c
@@ -143,6 +143,10 @@
 #  define MUNIT_NO_BUFFER
 #endif
 
+#if defined(CODE_COVERAGE_ENABLED)
+extern void __gcov_flush(void);
+#endif
+
 /*** Logging ***/
 
 static MunitLogLevel munit_log_level_visible = MUNIT_LOG_INFO;
@@ -1388,6 +1392,9 @@ munit_test_runner_run_test_with_params(MunitTestRunner* runner, const MunitTest*
           if (stderr_buf != NULL) {
             munit_log_errno(MUNIT_LOG_ERROR, stderr, "unable to write to pipe");
           }
+#if defined(CODE_COVERAGE_ENABLED)
+          __gcov_flush();
+#endif
           _exit(EXIT_FAILURE);
         }
         bytes_written += write_res;
@@ -1397,6 +1404,9 @@ munit_test_runner_run_test_with_params(MunitTestRunner* runner, const MunitTest*
         fclose(stderr_buf);
       close(pipefd[1]);
 
+#if defined(CODE_COVERAGE_ENABLED)
+      __gcov_flush();
+#endif
       _exit(EXIT_SUCCESS);
     } else if (fork_pid == -1) {
       close(pipefd[0]);
