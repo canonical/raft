569c32324db0fc2ad96dd57dbaaa368ab0d40b70
Mathieu Borderé <mathieu@bordere.org>
2022-03-24 11:58:27 +0100

    Revert "Async snapshot"

--- test/lib/fsm.h
+++ test/lib/fsm.h
@@ -7,10 +7,7 @@
 
 #include "../../include/raft.h"
 
-void FsmInit(struct raft_fsm *fsm, int version);
-
-/* Same as FsmInit but with asynchronous snapshots */
-void FsmInitAsync(struct raft_fsm *fsm, int version);
+void FsmInit(struct raft_fsm *fsm);
 
 void FsmClose(struct raft_fsm *fsm);
 
