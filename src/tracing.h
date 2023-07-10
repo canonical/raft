1eb5d367f73b49b9b7356f2346c62d7bb08daafe
Mathieu Borderé <mathieu.bordere@canonical.com>
2023-07-10 13:12:05 +0200

    utils.h: Add header file with utility macros.
    Similar to what's in dqlite.
    Use UNLIKELY macro in the tracer.
    
    Signed-off-by: Mathieu Borderé <mathieu.bordere@canonical.com>

--- src/tracing.h
+++ src/tracing.h
@@ -7,6 +7,7 @@
 #define LIBRAFT_TRACE "LIBRAFT_TRACE"
 
 #include "../include/raft.h"
+#include "utils.h"
 
 extern struct raft_tracer NoopTracer;
 
@@ -14,13 +15,14 @@ extern struct raft_tracer NoopTracer;
 extern struct raft_tracer StderrTracer;
 
 /* Emit a debug message with the given tracer. */
-#define Tracef(TRACER, ...)                                              \
-    do {                                                                 \
-        if (TRACER != NULL && TRACER->emit != NULL && TRACER->enabled) { \
-            static char _msg[1024];                                      \
-            snprintf(_msg, sizeof _msg, __VA_ARGS__);                    \
-            TRACER->emit(TRACER, __FILE__, __LINE__, _msg);              \
-        }                                                                \
+#define Tracef(TRACER, ...)                                    \
+    do {                                                       \
+        if (UNLIKELY(TRACER != NULL && TRACER->emit != NULL && \
+                     TRACER->enabled)) {                       \
+            static char _msg[1024];                            \
+            snprintf(_msg, sizeof _msg, __VA_ARGS__);          \
+            TRACER->emit(TRACER, __FILE__, __LINE__, _msg);    \
+        }                                                      \
     } while (0)
 
 /* Enable the tracer if the env variable is set or disable the tracer */
